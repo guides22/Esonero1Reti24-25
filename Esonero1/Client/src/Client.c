15138b03fe502a1d027d8ca0a4d32a0864a4bf77
Guillaume Andrea Desaphy <138069807+guides22@users.noreply.github.com>
2024-11-21 19:43:23 +0100

    Cartella per l'esonero 1
    Cartella contenente tutti i file della cartella Client e della cartella Server

---
+++ Esonero1/Client/src/Client.c
@@ -0,0 +1,98 @@
+/*
+ ============================================================================
+ Name        : Client.c
+ Author      : Guillaume Desaphy
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#if defined WIN32
+#include <winsock.h>
+#else
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#define closesocket close
+#endif
+#include <stdio.h>
+#include "protocol.h"
+
+void clearwinsock() {
+#if defined WIN32
+	WSACleanup();
+#endif
+}
+
+int main(int argc, char *argv[]) {
+#if defined WIN32
+	//Initialize Winsock
+	WSADATA wsa_data;
+	int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
+	if (result != NO_ERROR) {
+		printf("Error at WSAStartup().\n");
+		return 0;
+	}
+#endif
+	//client socket creation
+	int c_socket;
+	c_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (c_socket < 0) {
+		printf("Socket creation failed.\n");
+		closesocket(c_socket);
+		clearwinsock();
+		return -1;
+	}
+
+	//set connection settings
+	struct sockaddr_in sad;
+	memset(&sad, 0, sizeof(sad));
+	sad.sin_family = AF_INET;
+	sad.sin_addr.s_addr = inet_addr(PROTO_IP); //server IP
+	sad.sin_port = htons(PROTO_PORT); //server port
+	if (connect(c_socket,(struct sockaddr *) &sad, sizeof(sad)) < 0) {
+		printf( "Failed to connect.\n" );
+		closesocket(c_socket);
+		clearwinsock();
+		return -1;
+	}
+
+	printf("Connected with server.\n");
+	printf("Enter the command for type and length of password you want to generate('n','a','m' or 's' + length(>=6 and <=32))(enter 'q' to end): ");
+	char command[64];
+	scanf("%s", command);
+	while(command[0] != 'q') {
+		//send the command to server
+		printf("Sending command...\n");
+		if (send(c_socket, command, strlen(command), 0) != strlen(command)) {
+			printf("send() sent a different number of bytes than expected.\n");
+			closesocket(c_socket);
+			clearwinsock();
+			return -1;
+		}
+		printf("Command sent.\n");
+		//receive the password from server
+		char password[64];
+		memset(password, 0, sizeof(password)); //reset password
+		printf("Receiving password from server...\n");
+		if ((recv(c_socket, password, sizeof(password), 0)) <= 0) {
+			printf("recv() failed.\n");
+			closesocket(c_socket);
+			clearwinsock();
+			return -1;
+		}
+		printf("Password generated from server: %s.\n", password);
+		printf("Enter the command for type and length of password you want to generate('n','a','m' or 's' + length(>=6 and <=32))(enter 'q' to end): ");
+		scanf("%s", command);
+	}
+
+	//close connection
+	printf("Char 'q' entered, connection closed.\n");
+	closesocket(c_socket);
+	clearwinsock();
+	return 0;
+} // main end
---
+++ Esonero1/Client/src/protocol.h
@@ -0,0 +1,14 @@
+/*
+ * protocol.h
+ * Author: Guillaume Desaphy
+ *
+ */
+
+#ifndef PROTOCOL_H_
+#define PROTOCOL_H_
+
+#define NO_ERROR 0
+#define PROTO_IP "127.0.0.1"
+#define PROTO_PORT 57015
+
+#endif /* PROTOCOL_H_ */
