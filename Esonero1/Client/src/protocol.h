d92a5e2284de6b22ab47769e2117db81e802da5e
Guillaume Andrea Desaphy <138069807+guides22@users.noreply.github.com>
2024-11-22 12:08:51 +0100

    Cartella Esonero 1
    Cartella contenente i file del server e del client

---
+++ Esonero1/Client/src/protocol.h
@@ -0,0 +1,16 @@
+/*
+ * protocol.h
+ * Author: Guillaume Desaphy
+ *
+ */
+
+#ifndef PROTOCOL_H_
+#define PROTOCOL_H_
+
+#define NO_ERROR 0
+#define PROTO_IP "127.0.0.1"
+#define PROTO_PORT 57015
+#define MIN_PASSWORD_LENGTH 6
+#define MAX_PASSWORD_LENGTH 32
+
+#endif /* PROTOCOL_H_ */
